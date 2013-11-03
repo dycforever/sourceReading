f51f6523d16e9827c3c61cd2993ea6955831249a
dycforever <dycforever@126.com>
2013-11-03 23:06:35 +0000

    add ldd3 with comment

---
+++ linux-2.6.24/buildcscope.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+LNX=`pwd`
+
+find  $LNX                                                                \
+    -path "$LNX/arch/*" ! -path "$LNX/arch/i386*" -prune -o               \
+    -path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
+    -path "$LNX/tmp*" -prune -o                                           \
+    -path "$LNX/Documentation*" -prune -o                                 \
+    -path "$LNX/scripts*" -prune -o                                       \
+    -path "$LNX/drivers*" -prune -o                                       \
+    -name "*.[chxsS]" -print > ./cscope.files
+
+cscope -b -k -q
