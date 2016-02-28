65ce1d6b67031d2300683b8dfc1e6d4c4883274a
dycforever <dycforever@126.com>
2016-02-28 14:05:21 +0800

    rm linux-1.2.13

--- linux-1.2.13/include/asm-m68k/dma.h
+++
@@ -1,6 +0,0 @@
-#ifndef _M68K_DMA_H
-#define _M68K_DMA_H 1
-
-/* dummy for m68k */
-
-#endif /* _M68K_DMA_H */
--- linux-1.2.13/include/asm-m68k/irq.h
+++
@@ -1,6 +0,0 @@
-#ifndef _M68K_IRQ_H_
-#define _M68K_IRQ_H_
-
-/* dummy for m68k */
-
-#endif /* _M68K_IRQ_H_ */
